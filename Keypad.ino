7af5f4e5fb9c4601b4d2d05d98ca9a51941a530a
Erkhembileg Ariunbold <114377055+Lumb3@users.noreply.github.com>
2025-10-31 14:50:36 +0900

    Debug Code for the keypad working mechanism

--- Keypad.ino
+++ Keypad.ino
@@ -6,14 +6,15 @@ const byte ROWS = 4;
 const byte COLS = 4;
 
 const char keys[ROWS][COLS] = {
-  { '1', '2', '3', 'A' },
-  { '4', '5', '6', 'B' },
-  { '7', '8', '9', 'C' },
-  { '*', '0', '#', 'D' }
+  { '1', '2', '3', 'A' }, // 10 COLS
+  { '4', '5', '6', 'B' }, // 11
+  { '7', '8', '9', 'C' }, // 12
+  { '*', '0', '#', 'D' } // 13
+  // 2.   4.  7.   8 ROWS
 };
 
-byte rowPins[ROWS] = { 2, 4, 7, 8 };      // Hevtee (Yellow wires)
-byte colPins[COLS] = { 10, 11, 12, 13 };  // Vertical (Red wires)
+byte rowPins[ROWS] = { 2, 4, 7, 8 };      // Bosoo (Yellow wires)
+byte colPins[COLS] = { 10, 11, 12, 13 };  // Hevtee (Red wires)
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
@@ -24,12 +25,13 @@ void setup() {
 
 void loop() {
   char key = keypad.getKey();  // Check for pressed key
-  int INTkey;
+  int keyInt;
   if (key) {  // If any key is pressed
     if (key >= '0' && key <= '9') {
       keyInt = key - '0';   // Converting the character type data into integer
-      Serial.write(key);    // Send the key to FPGA through UART (Digital Output PIN: 1, Orange Wire)
-      Serial.println(key);  // (Optional) Print the data for debugging
+      //Serial.write(keyInt);    // Send the key to FPGA through UART (Digital Output PIN: 1, Orange Wire)
+      Serial.print("Sent: ");
+      Serial.println(keyInt);
     }
   }
 }
